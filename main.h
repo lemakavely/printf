131a9fb1d0abc62b3c4e72e59eb1ef0e8a03efb1
AsmaBaye <asmamaw.lejalem7@gmail.com>
2022-08-09 09:24:50 -0700

    readding C file

--- main.h
+++ main.h
@@ -2,7 +2,13 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
+<<<<<<< HEAD
+int _putchar(char c);
+int print_string(char *);
+int print_decimal(int value);
+=======
 
 int _putchar(char);
+>>>>>>> refs/remotes/origin/master
 
 #endif
